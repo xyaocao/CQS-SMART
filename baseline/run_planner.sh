2fc5bdc42679598933dff449c3478c3b3d2fd3c2
xyaocao <xinyao.cao@uzh.ch>
2025-11-10 18:38:43 +0100

    change path

--- baseline/run_planner.sh
+++ baseline/run_planner.sh
@@ -1,2 +1,2 @@
-python -m Exp.baseline.run_planner "question" "db_id" --dataset spider
-E:/Anaconda/envs/langchain_env/python.exe Exp/baseline/run_planner.py "question" "db_id" --dataset spider
+python -m baseline.run_planner "question" "db_id" --dataset spider
+E:/Anaconda/envs/langchain_env/python.exe baseline/run_planner.py "question" "db_id" --dataset spider
