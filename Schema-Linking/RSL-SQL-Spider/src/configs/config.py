7b8e046a58c1d5b3926a6da9651d890ab71bc52f
Xinyao Cao <xinyaoclaudia@gmail.com>
2026-03-24 14:43:03 +0100

    Update API key to placeholder
    Replaced the API key with a placeholder for security.

--- Schema-Linking/RSL-SQL-Spider/src/configs/config.py
+++ Schema-Linking/RSL-SQL-Spider/src/configs/config.py
@@ -3,6 +3,6 @@ dev_json_path = 'F:\MAster Thesis\CQS-SMART - 1\Data\spider_data\dev.json'
 # model='..' #gpt-4o or deepseek-coder
 model: str = "Qwen/Qwen2.5-Coder-14B-Instruct"
 # api = '..'
-api = "sk-rc-JD6IPL9QAVvuYWXPaGfL8Q"
+api = "YOUR-API-KEY"
 # base_url = '..'
 base_url = "https://api.swissai.cscs.ch/v1"
